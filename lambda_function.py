6c723fd98beb30496ceae237f78791fed94954af
karthik1691 <95345193+karthik1691@users.noreply.github.com>
2022-02-25 22:30:39 +0530

    updated function

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def lambda_handler(event, context):
-    d = {'col1' : [1,2], 'col2' : [3,4], 'col3' : [5,6]}
+    d = {'col1' : [1,2], 'col2' : [3,4], 'col3' : [5,6], 'col4' : [7,8]}
     df = pd.DataFrame{data=d}
     print(df)
     print('done xl')
