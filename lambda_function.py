dc10b437a24c6cc146677f5cf894fb28305d496e
karthik1691 <95345193+karthik1691@users.noreply.github.com>
2022-02-25 22:18:48 +0530

    updated lambda function.py
    has been updated on 25.02.2022 with few minor changes. please have a look into it

--- lambda_function.py
+++ lambda_function.py
@@ -1,8 +1,9 @@
 import pandas as pd
 
 def lambda_handler(event, context):
-    d = {'col1' : [1,2], 'col2' : [3,4]}
+    d = {'col1' : [1,2], 'col2' : [3,4], 'col3' :[5,6]}
     df = pd.DataFrame{data=d}
     print(df)
     print('done xl')
+   
 
